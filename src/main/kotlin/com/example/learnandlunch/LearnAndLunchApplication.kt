3a0f9f53f36b2ae0cc150ab9d26d5b7b5713861b
Maya Mutafova <maya.mutafova@gmail.com>
2017-12-05 21:50:27 +0100

    Premier commit => init du projet

---
+++ src/main/kotlin/com/example/learnandlunch/LearnAndLunchApplication.kt
@@ -0,0 +1,11 @@
+package com.example.learnandlunch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnAndLunchApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearnAndLunchApplication>(*args)
+}
