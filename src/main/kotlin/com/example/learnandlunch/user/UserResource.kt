0a3c2e89c364d0968a943f3f81e0ea2351f365b9
Erwan NEEL <erwan.neel@gmail.com>
2017-12-06 21:12:27 +0100

    Implémentation du post

--- src/main/kotlin/com/example/learnandlunch/user/UserResource.kt
+++ src/main/kotlin/com/example/learnandlunch/user/UserResource.kt
@@ -1,9 +1,8 @@
 package com.example.learnandlunch.user
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @RestController
 @RequestMapping("/api/v1")
@@ -17,8 +16,16 @@ class UserResource(
     @GetMapping("/users/{id}")
     fun getUserById(@PathVariable id: Int) = userService.findOne(id) ?: throw UserNotFoundException("id-$id")
 
-    fun createUser() {
-        // TODO: 1) implémenter la méthode createUser
+    @PostMapping("/users")
+    fun createUser(@RequestBody user: User): ResponseEntity<Any> {
+        val newUser = userService.save(user)
+        val locationUri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newUser.id)
+                .toUri()
+
+        return ResponseEntity.created(locationUri).build()
     }
 
     fun deleteUser() {
